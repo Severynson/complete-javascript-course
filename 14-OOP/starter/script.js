3d6dc9edb0387c85d08cb966bd8e1302b952a16c
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-01 16:54:29 +0200

    OOP part of course done! Yes!!!

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,96 +1,593 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-};
+// const Person = function (firstName, birthYear) {
+//   console.log(this);
+//   // this.species = 'Homo sapiens';
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+//   // Never do this!!! Very bad practice, duplicating code behind the scense.
+//   // this.calcAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// const severyn = new Person('Severyn', 2002);
+// console.log(severyn);
 
-// 1. New {} is created;
-// 2. function is called, this = {}
-// 3. {} linked to prototype;
-// 4. function automaticly return {}
+// // 1. New {} is created;
+// // 2. function is called, this = {};
+// // 3. {} Linked to prototype;
+// // 4. function automatically return {};
 
-const mathilda = new Person('Mathilda', 2017);
-const jack = new Person('Jack', 1975);
-console.log(mathilda, jack);
+// const jonas = new Person('Jonas', 1991);
+// const mathilda = new Person('Mathilda', 2017);
 
-console.log(jonas instanceof Person);
+// console.log(jonas);
+// console.log(mathilda);
 
-jonas.calcAge();
-mathilda.calcAge();
+// console.log(jonas instanceof Person);
+// console.log({} instanceof Person);
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(jonas));
+// // Prototypes
 
-// if to be honest , it is more like - .prototypeOfLinkedObj
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, mathilda.species);
+// console.log(Person.prototype);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+// jonas.calcAge();
+// severyn.calcAge();
 
-console.dir(Person.prototype.constructor);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
 
-const arr = [3, 4, 5, 3, 2, 6, 8];
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(mathilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// // Realy .prototype would be better to call more like: .prototypeOfLinkedObject
+// // So- prototype of prototype is not prototype )))
+// // console.log(Person.prototype.isPrototypeOf(Person));
 
-console.log(arr.unique());
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species)
+// console.log(jonas.species === mathilda.species);
 
-// Challenge #1
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
 
-const Car = function (model, speed) {
-  this.model = model;
-  this.speed = speed;
-};
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
 
-Car.prototype.break = function () {
-  this.speed -= 5;
-  console.log(`'${this.model}' going at ${this.speed} km/h`);
-};
+// console.dir(Person.prototype.constructor);
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`'${this.model}' going at ${this.speed} km/h`);
-};
+// const arr = [3, 3, 3, 5, 6];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ == Array.prototype);
 
-const bmw = new Car('BMW', 120);
-const mercedes = new Car('Mercedes', 95);
-bmw.accelerate();
-mercedes.accelerate();
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(h1);
+// console.dir(x => x + 1);
+
+// // Coding challenge 1
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+
+// Car.prototype.break = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.break();
+// mercedes.break();
+
+//////////////////////// ES6 Classes /////////////////////////////////
 
 // class expression
-// const Person = class {}
+// const personCl = class {};
 
 // class declaration
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  }
+//   // Methods will be added to .prototype property;
+//   calcAge() {
+//     console.log(2021 - this.birthYear);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     console.log(name);
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   static hey() {
+//     console.log('Hey there!');
+//     console.log(this);
+//   }
+// }
+
+// const jessica = new PersonCl('Jessica Davis', 1996);
+// console.log(jessica);
+// jessica.calcAge();
+
+// console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+// jessica.greet();
+
+// 1. Classes are not hoisted;
+// 2. Class are first-class citizens;
+// 3. Classes are executed in strict mode;
+
+// const account = {
+//   owner: 'jonas',
+//   movements: [200, 530, 120, 300],
+
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(mov) {
+//     this.movements.push(mov);
+//   }
+// };
+
+// console.log(account.latest);
+// console.log(account.movements);
+// account.latest = 50;
+// console.log(account.latest);
+// console.log(account.movements);
+// jessica.calcAge();
+// console.log(jessica.age);
+
+// const walter = new PersonCl('Walter White', 1965);
+
+// console.log(walter.fullName);
+// PersonCl.hey();
+// // walter.hey() will not work!;
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+
+// steven.birthYear = 2002;
+
+// steven.name = 'Steven';
+// console.log(steven);
+// steven.calcAge();
+
+/////////////////////////////////////////////////////////////
+
+// // Coding challenge 2
+
+// class Car {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+
+//   set speedUS(speed) {
+//     return (this.speed = speed * 1.6);
+//   }
+
+//   accelerate() {
+//     this.speed += 10;
+//     console.log(`Speed of ${this.make} was increased to ${this.speedUS}`);
+//   }
+
+//   break() {
+//     this.speed -= 5;
+//     console.log(`Speed of ${this.make} was decreased to ${this.speedUS}`);
+//   }
+// }
+
+// const ford = new Car('Ford', 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.break();
+// ford.speedUS = 50;
+// console.log(ford);
+
+
+// let number = 0;
+
+// const increaseForOne = () => {
+//   number++;
+//   console.log(number);
+// };
+
+// const decreaseForOne = () => {
+//   number--;
+//   console.log(number);
+// };
+
+// window.addEventListener('click', increaseForOne);
+// window.addEventListener('keypress', decreaseForOne);
+
+// ////////////// Inheritance Between "Classes": Constructor function;
+
+// const Person = function (firstNmae, birthYear) {
+//   this.firstName = firstNmae;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2021 - this.birthYear);
+// };
+
+// const Student = function(firstName, birthYear, course) {
+//   // this.firstName = firstName;
+//   // this.birthYear = birthYear;
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// // Linking prototypes;
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//   console.log(
+//     `My name is ${this.firstName} and I study ${this.course}`
+//   );
+// };
+
+// const mike = new Student('Mike', 2002, 'Computer Science');
+// console.log(mike);
+// mike.introduce();
+// mike.calcAge();
+
+// console.log(mike.__proto__);
+// console.log(mike.__proto__.__proto__);
+
+// console.log(mike instanceof Student);
+
+// Student.prototype.constructor = Student;
+// console.dir(Student.prototype.constructor);
+
+
+// // Coding challenge 3
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+
+// Car.prototype.break = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// // Link the prototype
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeTo) {
+//   this.charge = `${chargeTo}%`;
+// };
+
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   this.charge--;
+//   console.log(`After accelerate speed is ${this.speed}km/h and charge is ${this.charge}`);
+// };
+
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.break();
+// mercedes.break();
+
+// const tesla = new EV('Tesla', 666, '100%');
+// console.log(tesla);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.break();
+// tesla.accelerate();
+// tesla.accelerate();
+// tesla.accelerate();
+
+/////////////////////////////////////////////////////////////////////////
+
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   // Methods will be added to .prototype property;
+//   calcAge() {
+//     console.log(2021 - this.birthYear);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   static hey() {
+//     console.log('Hey there!');
+//     console.log(this);
+//   }
+// }
+
+// class StudentCl extends PersonCl {
+//   constructor(fullName, birthYear, course) {
+//     super(fullName, birthYear);
+//     course = this.course;
+//   }
+
+//   introduce() {
+//     console.log(`My name is ${this.fullName} and I study ${this.course}`)
+//   }
+
+//   calcAge() {
+//     console.log(`I'm ${2021 - this.birthYear} years old, but as student I feel more like ${2021 - this.birthYear + 10}`)
+//   }
+// }
+
+// const martha = new StudentCl('Martha Jones', 2002, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear)
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steve = Object.create(PersonProto);
+
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (firstName, birthYear, course) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// StudentProto.introduce = function () {
+//   console.log(`Hello, my name is ${this.firstName} and I study ${this.course}!`)
+// };
+
+// const jay = Object.create(StudentProto);
+// jay.init('Jay', 1992, 'Computer science');
+// console.log(jay);
+// jay.introduce();
+// jay.calcAge();
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// class Account {
+//   // 1) Public fields;
+//   locale = navigator.language;
+  
+//   // 2) Private fields;
+//   #movements = [];
+//   #pin;
+
+//   constructor(owner, currency, pin) {
+//     this.owner = owner;
+//     this.currency = currency;
+//     this.#pin = pin;
+//     // this._movements = [];
+//     // this.locale = navigator.language;
+//     console.log(`Thanks for opening in account, ${owner}`);
+//   };
+
+//   // (App) Public Interface;
+//   getMovements() {
+//     return this.#movements;
+//   };
+
+//   deposit(val) {
+//     this.#movements.push(val);
+//     return this;
+//   };
+
+//   withdraw(val) {
+//     this.deposit(-val)
+//     return this;
+//   };
+
+//   // _approveLoan(val) {
+//   //   return true;
+//   // };
+
+//   requestLoan(val) {
+//     if (this.#approveLoan(val)) {
+//       this.deposit(val);
+//       console.log(`Loan approved`);
+//       return this;
+//     }
+//   }
+
+//   // It works just in classes!!
+//   static helper() {
+//     console.log('Helper');
+//   };
+
+//   // Private methods works in all parts of code;
+//   #approveLoan(val) {
+//     return true;
+//   };
+// };
+
+// const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// // acc1.movements.push(250);
+// // acc1.movements.push(-140);
+// acc1.deposit(250);
+// acc1.withdraw(140);
+// acc1.requestLoan(1000);
+// // Of course acc1.#approveLoan() doesn't work;
+// // And this will not work the same -> console.log(acc1.#movements);
+// // acc1.approveLoan(1000);
+// const call1 = acc1.getMovements();
+// console.log(call1);
+
+// console.log(acc1);
+// console.log(acc1.pin);
+
+
+// // Chaining
+// acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+// console.log(acc1.getMovements());
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Coding challenge 3
+
+class CarCl {
+
+ constructor (make, speed) {
+  this.make = make;
+  this.speed = speed;
+
+
+};
+
+accelerate() {
+  this.speed += 10;
+  console.log(this.speed);
+  return this;
+};
+
+break() {
+  this.speed -= 5;
+  console.log(this.speed);
+  return this;
+};
+
+get speedUS () {
+  return this.speed / 1.6;
 }
 
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
-jessica.calcAge();
+set speedUS (speed) {
+  console.log(`I forgot an algorithm that must to be here))) Too lazy for search right now, sorry))`)
+}
+};
 
-// 1.Classes are NOT hoisted;
-// 2.Classes are first-class citizens;
-// 3.Classes are executed in strict mode.
+
+class EVCl extends CarCl {
+  #charge;
+
+constructor (make, speed, charge) {
+  super( make, speed);
+  this.#charge = charge;
+};
+
+        // Happens automaticly;
+  // EV.prototype = Object.create(Car.prototype);
+
+chargeBattery(chargeTo) {
+  this.#charge = chargeTo;
+  return this;
+};
+
+accelerate() {
+  this.speed += 20;
+  this.#charge--;
+  console.log(`After accelerate speed is ${this.speed}km/h and charge is ${this.charge}`);
+  return this;
+};
+};
+
+// Link the prototype
+
+
+
+
+
+const bmw = new CarCl('BMW', 120);
+const mercedes = new CarCl('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.accelerate();
+bmw.break();
+mercedes.break();
+
+const tesla = new EVCl('Tesla', 666, 100);
+console.log(tesla);
+console.log(tesla.speedUS);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.break();
+tesla.accelerate().accelerate().accelerate().break().chargeBattery(50).accelerate();
+// Of course doesn't work -> console.log(tesla.#charge)
--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -20,10 +20,10 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const renderError = (msg) => {
+const renderError = msg => {
   countriesContainer.insertAdjacentHTML('beforeend', msg);
   countriesContainer.style.opacity = 1;
-}
+};
 
 ///////////////////////////////////////
 
@@ -57,7 +57,8 @@ const renderError = (msg) => {
 //     renderCountry(data);
 
 //     // Get neighbour country (2)
-//     const [neighbour] = data.borders;
+//     const
+//  [neighbour] = data.borders;
 
 //     if(!neighbour) return;
 
@@ -80,26 +81,242 @@ const renderError = (msg) => {
 
 //////////////////////////////////////////////
 
-const getCountryData = function (country) {
-  // Country 1
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
-
-      if (!neighbour) return;
-
-      // Country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
-    })
-    .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbour'))
-    .catch(err => {console.error(`${err} ?????? !!!`)
-    renderError(`Something went wrong?????? ${err}`)
+const getJSON = function (url, errorMsg = 'Something went wrong!') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
   });
 };
 
-btn.addEventListener('click', () => {
-  getCountryData('portugal');
-});
+// const getCountryData = function (country) {
+//   // Country 1
+//   getJSON(
+//     `https://restcountries.com/v3.1/name/${country}`,
+//     'Country not found!'
+//   )
+//     // fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     //   .then(response => {
+//     //     console.log(response);
+//     //     if (!response.ok)
+//     //       throw new Error(`Country not found (${response.status})`);
+//     //     return response.json();
+//     //   })
+
+//     .then(data => {
+//       console.log(data[0].borders[0]);
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       console.log(neighbour);
+
+//       if (!neighbour) throw new Error('No neighbour found!');
+
+//       // Country 2
+//       // return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//       return getJSON(
+//         `https://restcountries.com/v3.1/alpha/${neighbour}`,
+//         'Neighbour country not found!'
+//       );
+//     });
+
+//   // .then(data => renderCountry(data[0], 'neighbour'))
+//   // .catch(err => {
+//   //   console.error(`${err} ?????? !!!`);
+//   //   renderError(`Something went wrong!!! ${err}. Try again!`);
+//   // })
+//   // .finally(() => {
+//   //   countriesContainer.style.opacity = 1;
+//   // });
+// };
+
+// btn.addEventListener('click', () => {
+//   // getCountryData('portugal');
+//   getCountryData('australia');
+// });
+
+// // getCountryData('porrfertureerferfal');
+
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+
+//       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+//       return res.json();
+//     })
+//     .then(data => renderCountry(data[0]))
+//     .catch(err => console.error(`${err.message} 💥`));
+// };
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+// Second API dosn't work for a moment of my course learning. So I decided to just watch a videos and have practice another time. I'm tired anough last days to writing code.
+
+////////////////////////////////////////////////////////////////////
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lottery draw is happening...')
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You win!');
+//     } else {
+//       reject( new Error('You lost your money!'));
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// // Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// wait(2).then(() => {
+//   console.log('I waited for two seconds')
+//   return wait(1);
+// }).then(() => {
+//   console.log('I waited for 1 second')
+// })
+
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+/////////////////////////////////////////////////////////////////////////
+
+// const getPosition = function() {
+//   return new Promise(function(resolve, reject) {
+// //     navigator.geolocation.getCurrentPosition(
+// //   possition => resolve(possition),
+// //   err => reject(err)
+// // );
+//     navigator.geolocation.getCurrentPosition(resolve, reject)
+//   })
+// }
+
+// getPosition().then(pos => console.log(pos));
+
+// const blabla = (imgPath) => {
+//   return new Promise(function(resolve, rejsect) {
+//     countriesContainer.style.opacity = 1;
+//     const img = document.createElement('img');
+//     img.src = imgPath;
+
+//     img.addEventListener('load', () => {
+//       countriesContainer.append(img);
+//       resolve(img);
+//     })
+
+//     img.addEventListener('error', () => {
+//       rejsect(new Error('Image not found!'))
+//     })
+
+//   })
+// }
+
+// let currentImg;
+
+// blabla("./img/img-1.jpg").then(img => {
+
+//   console.log('Img 1 was loaded');
+//   return wait(2)
+// })
+// .then(() => {
+//   countriesContainer.style.opacity = 0;
+// })
+// .then(() => {return wait(2)})
+// .then(() => {
+//   return blabla("./img/img-2.jpg")
+// })
+// .then(() => {
+
+//   console.log('Img 2 was loaded');
+//   return wait(2)
+// }).then(() => {
+//   countriesContainer.style.opacity = 0;
+// })
+// .then(() => {return wait(2)})
+// .then(() => {
+//   return blabla("./img/img-3.jpg")
+// })
+// .then(() => console.log('Img 3 was loaded'))
+// .catch(err => console.error(err))
+
+// const whereAmI = async function (country) {
+//   const data = await fetch(`https://restcountries.com/v3.1/name/portugal`);
+//   console.log(data.json());
+
+//   // The same result as:
+
+//   fetch(`https://restcountries.com/v3.1/name/portugal`).then(data =>
+//     console.log(data.json())
+//   );
+// };
+
+// whereAmI();
+
+// Promise.race
+// (async function () {
+//   const res = await Promise.race([
+//     getJSON(`https://restcountries.com/v3.1/name/portugal`),
+//     getJSON(`https://restcountries.com/v3.1/name/ukraine`),
+//     getJSON(`https://restcountries.com/v3.1/name/poland`),
+//   ]);
+//   console.log(res[0])
+// })();
+
+const timeout = function (sec) {
+  return new Promise(function (_, rejsect) {
+    setTimeout(() => {
+      rejsect(new Error('request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/portugal`),
+  timeout(0.3),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.log(err));
+
+// Promise.allSettled
+
+const promiseAwait = async () => {
+  return Promise.all([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+  ]);
+};
+
+promiseAwait().then(res => {console.log(res)
+// res.map((el) => {
+//   if (el.status === 'rejected') {
+//     console.log(el)
+//   }
+// })
+}).catch((err) => console.error(`uuuuuuuuuu ${err}`))
+
+// Promise.any
+
+const promiseAwait2 = async () => {
+  return Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another success'),
+  ]);
+};
+
+promiseAwait2().then(res => console.log(res));
